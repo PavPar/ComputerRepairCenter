fb0444c35984cf5e70f2d68853431e7daddcf5f8
PavPar <ParamonPav@yandex.ru>
2020-06-11 02:34:25 +0300

    Fixed headers

--- PHP/value-view.php
+++ PHP/value-view.php
@@ -3,7 +3,28 @@
 <?php checkPrivalge()?>
 <!DOCTYPE html>
 <html lang="ru">
+<?php 
+function createHeader()
+{
+    require_once 'template.php';
+    $parse = new parse_class;
+    if (userCheck()) {
+        if (checkPrivalge()) {
+            $parse->get_tpl('index__header-admin.tpl');
+            $parse->set_tpl('{LOGIN}', getUserData()['login']);
+        } else {
+            $parse->get_tpl('index__header-master.tpl');
+            $parse->set_tpl('{LOGIN}', getUserData()['login']);
+        }
 
+    } else {
+        echo "Auth error!";
+        return;
+    }
+    $parse->tpl_parse();
+    echo $parse->template;
+}
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,16 +33,7 @@
 </head>
 
 <body class="page">
-    <header class="header">
-        <div class="acc-info">
-            <img class="acc-info__logo" src="../images/inqusition-01.svg">
-            <h2 class="acc-info__name">Master_1</h2>
-            <form method="POST">
-            <button class="btn btn_type-user" value="master" formaction="admin.php">Страница Пользователя</button>
-            <button  class="btn btn_type-logout" value="logout" formaction="logout.php">Выйти из системы</button>
-            </form>
-        </div>
-    </header>
+<?php createHeader();?>
     <main class="content">
     <div class="ticket-info">
             <table class="ticket-info__table">
